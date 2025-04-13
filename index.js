aa78f1045950f29d7dac2e4b9ddb55fca56fde13
vanshbothra <67708601+vanshbothra@users.noreply.github.com>
2025-04-13 18:20:31 +0530

    add show and hide buttons

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-document.getElementById("contact-button").addEventListener("click",function(){
-    alert("Hello this is an alert!");
+document.getElementById("hide-button").addEventListener("click",function(){
+    document.getElementById("hide-element").style.display="none";
+    document.getElementById("show-button").style.display="block";
+    document.getElementById("hide-button").style.display="none";
+    // alert("Hello this is an alert!");
+    // document.body.style="background-color:white";
+});
+
+document.getElementById("show-button").addEventListener("click",function(){
+    document.getElementById("hide-element").style.display="block";
+    document.getElementById("hide-button").style.display="block";
+    document.getElementById("show-button").style.display="none";
+    // alert("Hello this is an alert!");
     // document.body.style="background-color:white";
 });
